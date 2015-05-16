f3fe698b96c0810dfd43b1edadd5f96132a00b88
Alessandro <ale.fog93@yahoo.it>
2015-05-16 17:46:45 +0200

    add Create e Get ArduinoBoard

---
+++ src/it/uniroma3/iota/controller/action/CreateArduinoBoard.java
@@ -0,0 +1,33 @@
+package it.uniroma3.iota.controller.action;
+
+
+import it.uniroma3.iota.controller.helper.HelperArduinoBoard;
+import it.uniroma3.iota.model.ArduinoBoard;
+import it.uniroma3.iota.model.ArduinoBoardFacade;
+
+import javax.ejb.EJB;
+import javax.servlet.http.HttpServletRequest;
+
+public class CreateArduinoBoard implements Action{
+	
+	@EJB(beanName="aFacade")
+	private ArduinoBoardFacade facade;
+
+	public String perform(HttpServletRequest request) {
+
+		HelperArduinoBoard helper = new HelperArduinoBoard();
+
+		if (helper.isValid(request)) {
+			String code = request.getParameter("arduinoBoardCode");
+
+			
+			ArduinoBoard arduinoBoard = facade.createArduinoBoard(code);
+			request.setAttribute("arduinoBoard", arduinoBoard);
+
+			return "/arduinoBoard.jsp";
+		} else
+			return "/newArduinoBoard.jsp";
+	}
+
+
+}
