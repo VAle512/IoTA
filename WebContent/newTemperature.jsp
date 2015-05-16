f0cc00b40ba6a72a1fb614a2837965d3c9677013
valerio <valerio_cetorelli@yahoo.it>
2015-05-16 17:49:00 +0200

    added CreateTemperature

---
+++ WebContent/newTemperature.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Inserimento Temperatura</title>
+</head>
+<body>
+<h1>Inserimento nuova Temperatura</h1>
+	<form action="productController" method="get">
+		<div>
+			Id Board: <input type="text" name="arduinoBoardId" value="${temperature.board.id}" />
+			<font color = "red" >${arduinoBoardIdErr} </font>
+		</div>
+		<div>
+			Value: <input type="text" name="temperatureValue" value="${temperature.value}"/>
+			 ${temperatureValueErr}
+		</div>
+		<div>
+			Evaluation Time: <input type="text" name="temperatureEvaluationTime" value = "${temperature.evaluationTime}"/>
+			 ${temperatureEvaluationTimeErr}
+		</div>
+		<div>
+			<input type="submit" name="sumbit" value="invia" />
+		</div>
+	</form>
+</body>
+</html>
--- src/it/uniroma3/iota/controller/action/CreateTemperature.java
+++ src/it/uniroma3/iota/controller/action/CreateTemperature.java
@@ -1,28 +1,38 @@
 package it.uniroma3.iota.controller.action;
 
-import it.uniroma3.iota.controller.helper.HelperTemperatureId;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import it.uniroma3.iota.controller.helper.HelperTemperature;
+import it.uniroma3.iota.controller.helper.HelperTemperatureId;
+import it.uniroma3.iota.model.ArduinoBoard;
+import it.uniroma3.iota.model.Temperature;
+import it.uniroma3.iota.model.TemperatureFacade;
+
+import javax.ejb.EJB;
 import javax.servlet.http.HttpServletRequest;
 
 public class CreateTemperature {
+	
+	@EJB(beanName="tFacade")
+	private TemperatureFacade tf;
 
 	public String perform(HttpServletRequest request) {
 
 		HelperTemperature helper = new HelperTemperature();
 
 		if (helper.isValid(request)) {
-			String name = request.getParameter("name");
-			String code = request.getParameter("code");
-			Float price = Float.parseFloat(request.getParameter("price"));
-			String description = request.getParameter("description");
-
-			ProductFacade facade = new ProductFacade();
-			Product product = facade.createProduct(name, code, price, description);
-			request.setAttribute("product", product);
-
-			return "/product.jsp";
+			Double value = Double.parseDouble(request.getParameter("temperatureValue"));
+			SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+			Date evaluationTime = format.parse(request.getParameter("temperatureEvaluationTime"));
+			GetArduinoBoard = new GetArduinoBoard();
+			ArduinoBoard board = GetArduinoBoard.perform(request);
+			
+			Temperature temperature = this.tf.createTemperature(board, value, evaluationTime);
+			request.setAttribute("temperature", temperature);
+			return "/temperature.jsp";
 		} else
-			return "/newProduct.jsp";
+			return "/newTemperature.jsp";
 	}
 
 }
